d7fc5a429f003f5965e1985d5a9c91d69ac35894
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-03-07 08:51:45 +0500

    Update dictionary.py to suitable names

--- structures/Dictionaries/dictionary.py
+++ structures/Dictionaries/dictionary.py
@@ -2,8 +2,8 @@ YourMom = [0] * 100
 #First Approach
 Dictionary = {}  #initialised
 
-Dictionary["Aitzaz"] = "Bitchy Man"
-Dictionary["Arsla"] = "an irresistably cute penguin"
+Dictionary["Aitzaz"] = "Female"
+Dictionary["Arsla"] = "Male"
 
 print(Dictionary)
 print(Dictionary["Arsla"])
